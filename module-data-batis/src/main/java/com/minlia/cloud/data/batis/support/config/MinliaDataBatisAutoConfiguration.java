85d62ca41e4da55956aa1087edc652c1bacd2d04
will <will@will.com>
2017-08-01 13:44:40 +0800

    Update at 20170801134440 by dev/will

--- module-data-batis/src/main/java/com/minlia/cloud/data/batis/support/config/MinliaDataBatisAutoConfiguration.java
+++ module-data-batis/src/main/java/com/minlia/cloud/data/batis/support/config/MinliaDataBatisAutoConfiguration.java
@@ -1,6 +1,5 @@
 package com.minlia.cloud.data.batis.support.config;
 
-import com.minlia.cloud.autoconfiguration.MinliaCloudAutoConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -10,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mybatis.domains.AuditDateAware;
 import org.springframework.data.mybatis.replication.transaction.ReadWriteManagedTransactionFactory;
 import org.springframework.data.mybatis.repository.config.EnableMybatisRepositories;
