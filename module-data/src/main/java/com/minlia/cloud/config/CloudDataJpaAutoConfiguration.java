3df7a7030b6cd1398726ffb7cd90a1f806e53456
will <will@will.com>
2017-08-13 03:19:11 +0800

    Update at 20170813031910 by dev/will

--- module-data/src/main/java/com/minlia/cloud/config/CloudDataJpaAutoConfiguration.java
+++ module-data/src/main/java/com/minlia/cloud/config/CloudDataJpaAutoConfiguration.java
@@ -1,10 +1,16 @@
 package com.minlia.cloud.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
@@ -41,23 +47,21 @@ public class CloudDataJpaAutoConfiguration {
      * JPA Configuration
      */
     @EntityScan(basePackages = {".**.domain", ".**.model"})
-//    @EnableJpaRepositories(value = {".**.repository"}, considerNestedRepositories = true, transactionManagerRef = "jpaTransactionManager")
+    @EnableJpaRepositories(value = {".**.repository"}, considerNestedRepositories = true, transactionManagerRef = "jpaTransactionManager")
 //    @EnableJpaAuditing(auditorAwareRef = "auditorAware")
     @Configuration
     @ConditionalOnClass(CloudDataJpaAutoConfiguration.class)
-
     public static class EnableMinliaCloudJpaRepository {
-//
-//        @Autowired
-//        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean;
-//
-//        @Primary
-//        @Bean
-//        public JpaTransactionManager jpaTransactionManager() {
-//            JpaTransactionManager transactionManager = new JpaTransactionManager();
-//            transactionManager.setEntityManagerFactory(localContainerEntityManagerFactoryBean.getObject());
-//            return transactionManager;
-//        }
+        @Autowired
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean;
+
+        @Primary
+        @Bean
+        public JpaTransactionManager jpaTransactionManager() {
+            JpaTransactionManager transactionManager = new JpaTransactionManager();
+            transactionManager.setEntityManagerFactory(localContainerEntityManagerFactoryBean.getObject());
+            return transactionManager;
+        }
     }
 
 
