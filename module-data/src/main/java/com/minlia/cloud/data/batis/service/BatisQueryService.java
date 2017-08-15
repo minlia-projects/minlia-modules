51be3889f6d970385e449f1a5d7df1d9e5495bed
will <will@will.com>
2017-08-15 23:34:42 +0800

    Update at 20170815233442 by dev/will

--- module-data/src/main/java/com/minlia/cloud/data/batis/service/BatisQueryService.java
+++ module-data/src/main/java/com/minlia/cloud/data/batis/service/BatisQueryService.java
@@ -24,84 +24,84 @@ public interface BatisQueryService<REPOSITORY extends BatisDao<ENTITY, PK>,
         ENTITY extends Persistable<PK>, PK extends Serializable> {
 
 
-//    public Page<ENTITY> findBasePage(Pageable pageable, SpecificationDetail<ENTITY> specificationDetail, Boolean isBasicPropertyLimited, String selectStatement, String countStatement);
-//
-//    public Page<ENTITY> findPageQuery(Pageable pageable, List<QueryCondition> authQueryConditions, Boolean isBasicPropertyLimited);
-//
-//    public Page<ENTITY> findBasePage(Pageable pageable, SpecificationDetail<ENTITY> specificationDetail, Boolean isBasicPropertyLimited);
-//
-//    public List<ENTITY> findAll(List<QueryCondition> authQueryConditions, Boolean isBasicPropertyLimited);
-//
-//    public List<ENTITY> findAll(SpecificationDetail<ENTITY> specificationDetail, Boolean isBasicPropertyLimited);
-
-
-    public Pageable constructPageable(Pageable pageable);
+    public Page<ENTITY> findBasePage(Pageable pageable, SpecificationDetail<ENTITY> specificationDetail, Boolean isBasicPropertyLimited, String selectStatement, String countStatement);
 
     public Page<ENTITY> findPageQuery(Pageable pageable, List<QueryCondition> authQueryConditions, Boolean isBasicPropertyLimited);
 
-    public Page<ENTITY> findAll(Pageable pageable, SpecificationDetail<ENTITY> specificationDetail, Boolean isBasicPropertyLimited);
+    public Page<ENTITY> findBasePage(Pageable pageable, SpecificationDetail<ENTITY> specificationDetail, Boolean isBasicPropertyLimited);
+
+    public List<ENTITY> findAll(List<QueryCondition> authQueryConditions, Boolean isBasicPropertyLimited);
+
+    public List<ENTITY> findAll(SpecificationDetail<ENTITY> specificationDetail, Boolean isBasicPropertyLimited);
 
 
-    /**
-     * 动态分页查询
-     *
-     * @param pageable               分页对象
-     * @param specificationDetail    动态条件对象
-     * @param isBasicPropertyLimited 是否关联对象查询
-     * @param selectStatement        自定义数据集合sql名称
-     * @param countStatement         自定义数据总数sql名称
-     * @return
-     */
-
-    public Page<ENTITY> findBasePage(Pageable pageable, SpecificationDetail<ENTITY> specificationDetail, Boolean isBasicPropertyLimited, String selectStatement, String countStatement);
-
-    public List<ENTITY> findAll(List<QueryCondition> conditions, Boolean isBasicPropertyLimited);
-
-    @Transactional(readOnly = true)
-    public List<ENTITY> findAll(SpecificationDetail specificationDetail, Boolean isBasicPropertyLimited);
-
-
-    public boolean doCheckWithEntity(ENTITY entity, Map<String, QueryOperator> maps);
-
-    public boolean doCheckByProperty(ENTITY entity);
-
-    public boolean doCheckByPK(ENTITY entity);
-
-    public Iterable<ENTITY> save(Iterable<ENTITY> entitys);
-
-    public Iterable<ENTITY> saveIgnoreNull(Iterable<ENTITY> entitys);
-
-    public ENTITY saveIgnoreNull(ENTITY entity);
-
-    public ENTITY save(ENTITY entity);
-
-
-    @Transactional(readOnly = true)
-    public ENTITY findOne(PK id);
-
-    @Transactional(readOnly = true)
-    public Optional<ENTITY> findOneById(PK id);
-
-
-    public ENTITY findOne(Map<String, Object> paramsMap, String... columns);
-
-    public List<ENTITY> findAll(Map<String, Object> paramsMap, String... columns);
-
-    public List<ENTITY> findAll(Sort sort, Map<String, Object> paramsMap, String... columns);
-
-    public Page<ENTITY> findAll(Pageable pageable, Map<String, Object> paramsMap, String... columns);
-
-    public Long countBasicAll(Map<String, Object> paramsMap);
-
-
-    /**
-     * 动态集合查询
-     *
-     * @param specificationDetail 动态条件对象
-     * @return
-     */
-    @Transactional(readOnly = true)
-    public List<ENTITY> findAll(SpecificationDetail specificationDetail);
+//    public Pageable constructPageable(Pageable pageable);
+//
+//    public Page<ENTITY> findPageQuery(Pageable pageable, List<QueryCondition> authQueryConditions, Boolean isBasicPropertyLimited);
+//
+//    public Page<ENTITY> findAll(Pageable pageable, SpecificationDetail<ENTITY> specificationDetail, Boolean isBasicPropertyLimited);
+//
+//
+//    /**
+//     * 动态分页查询
+//     *
+//     * @param pageable               分页对象
+//     * @param specificationDetail    动态条件对象
+//     * @param isBasicPropertyLimited 是否关联对象查询
+//     * @param selectStatement        自定义数据集合sql名称
+//     * @param countStatement         自定义数据总数sql名称
+//     * @return
+//     */
+//
+//    public Page<ENTITY> findBasePage(Pageable pageable, SpecificationDetail<ENTITY> specificationDetail, Boolean isBasicPropertyLimited, String selectStatement, String countStatement);
+//
+//    public List<ENTITY> findAll(List<QueryCondition> conditions, Boolean isBasicPropertyLimited);
+//
+//    @Transactional(readOnly = true)
+//    public List<ENTITY> findAll(SpecificationDetail specificationDetail, Boolean isBasicPropertyLimited);
+//
+//
+//    public boolean doCheckWithEntity(ENTITY entity, Map<String, QueryOperator> maps);
+//
+//    public boolean doCheckByProperty(ENTITY entity);
+//
+//    public boolean doCheckByPK(ENTITY entity);
+//
+//    public Iterable<ENTITY> save(Iterable<ENTITY> entitys);
+//
+//    public Iterable<ENTITY> saveIgnoreNull(Iterable<ENTITY> entitys);
+//
+//    public ENTITY saveIgnoreNull(ENTITY entity);
+//
+//    public ENTITY save(ENTITY entity);
+//
+//
+//    @Transactional(readOnly = true)
+//    public ENTITY findOne(PK id);
+//
+//    @Transactional(readOnly = true)
+//    public Optional<ENTITY> findOneById(PK id);
+//
+//
+//    public ENTITY findOne(Map<String, Object> paramsMap, String... columns);
+//
+//    public List<ENTITY> findAll(Map<String, Object> paramsMap, String... columns);
+//
+//    public List<ENTITY> findAll(Sort sort, Map<String, Object> paramsMap, String... columns);
+//
+//    public Page<ENTITY> findAll(Pageable pageable, Map<String, Object> paramsMap, String... columns);
+//
+//    public Long countBasicAll(Map<String, Object> paramsMap);
+//
+//
+//    /**
+//     * 动态集合查询
+//     *
+//     * @param specificationDetail 动态条件对象
+//     * @return
+//     */
+//    @Transactional(readOnly = true)
+//    public List<ENTITY> findAll(SpecificationDetail specificationDetail);
 
 
 }
