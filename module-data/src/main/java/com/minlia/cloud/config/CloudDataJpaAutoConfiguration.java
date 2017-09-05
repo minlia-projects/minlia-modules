7e33441efbae77515722f08f75c979f24a516f2a
will <will@minlia.com>
2017-09-06 03:47:45 +0800

    Update at 20170906034742 by dev/will

--- module-data/src/main/java/com/minlia/cloud/config/CloudDataJpaAutoConfiguration.java
+++ module-data/src/main/java/com/minlia/cloud/config/CloudDataJpaAutoConfiguration.java
@@ -1,9 +1,5 @@
 package com.minlia.cloud.config;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Locale;
 import javax.sql.DataSource;
 import liquibase.integration.spring.SpringLiquibase;
 import lombok.extern.slf4j.Slf4j;
@@ -17,88 +13,81 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.auditing.DateTimeProvider;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.threeten.bp.DateTimeUtils;
-import org.threeten.bp.ZonedDateTime;
 
-
+/**
+ * Minlia Data Auto Configuration
+ */
 @ComponentScan(basePackages = "com.minlia.*")
 @Configuration
 @ConditionalOnClass(com.minlia.cloud.autoconfiguration.MinliaCloudAutoConfiguration.class)
 @Slf4j
-/**
- * Minlia Data Auto Configuration
- */
 @EnableTransactionManagement
 public class CloudDataJpaAutoConfiguration {
 
 
-    /**
-     * JPA Configuration
-     */
-    @EntityScan(basePackages = {".**.domain", ".**.model","org.springframework.data.jpa.convert.threeten"})//,basePackageClasses = Jsr310JpaConverters.class
-    @EnableJpaRepositories(value = {".**.repository"}, considerNestedRepositories = true, transactionManagerRef = "jpaTransactionManager")
-    @EnableJpaAuditing
-    @Configuration
-    @ConditionalOnClass(CloudDataJpaAutoConfiguration.class)
-    public static class EnableMinliaCloudJpaRepository {
-        @Autowired
-        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean;
-
-        @Primary
-        @Bean
-        public JpaTransactionManager jpaTransactionManager() {
-            JpaTransactionManager transactionManager = new JpaTransactionManager();
-            transactionManager.setEntityManagerFactory(localContainerEntityManagerFactoryBean.getObject());
-            return transactionManager;
-        }
-
-
-
-    }
-
-
-
+  /**
+   * JPA Configuration
+   */
+  @EntityScan(basePackages = {".**.domain", ".**.model",
+      "org.springframework.data.jpa.convert.threeten"})
+//,basePackageClasses = Jsr310JpaConverters.class
+  @EnableJpaRepositories(value = {
+      ".**.repository"}, considerNestedRepositories = true, transactionManagerRef = "jpaTransactionManager")
+  @EnableJpaAuditing
+  @Configuration
+  @ConditionalOnClass(CloudDataJpaAutoConfiguration.class)
+  public static class EnableMinliaCloudJpaRepository {
 
     @Autowired
-    private DataSource dataSource;
+    LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean;
 
+    @Primary
     @Bean
-    public LiquibaseProperties liquibaseProperties() {
-        return new LiquibaseProperties();
+    public JpaTransactionManager jpaTransactionManager() {
+      JpaTransactionManager transactionManager = new JpaTransactionManager();
+      transactionManager
+          .setEntityManagerFactory(localContainerEntityManagerFactoryBean.getObject());
+      return transactionManager;
     }
+  }
 
-    @Bean
-    @DependsOn(value = "entityManagerFactory")
-    public SpringLiquibase liquibase() {
-        LiquibaseProperties liquibaseProperties = liquibaseProperties();
-        SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setChangeLog(liquibaseProperties.getChangeLog());
-        liquibase.setContexts(liquibaseProperties.getContexts());
-        liquibase.setDataSource(getDataSource(liquibaseProperties));
-        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
-        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
-        liquibase.setShouldRun(true);
-        liquibase.setLabels(liquibaseProperties.getLabels());
-        liquibase.setChangeLogParameters(liquibaseProperties.getParameters());
-        return liquibase;
+  @Autowired
+  private DataSource dataSource;
+
+  @Bean
+  public LiquibaseProperties liquibaseProperties() {
+    return new LiquibaseProperties();
+  }
+
+  @Bean
+  @DependsOn(value = "entityManagerFactory")
+  public SpringLiquibase liquibase() {
+    LiquibaseProperties liquibaseProperties = liquibaseProperties();
+    SpringLiquibase liquibase = new SpringLiquibase();
+    liquibase.setChangeLog(liquibaseProperties.getChangeLog());
+    liquibase.setContexts(liquibaseProperties.getContexts());
+    liquibase.setDataSource(getDataSource(liquibaseProperties));
+    liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+    liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+    liquibase.setShouldRun(true);
+    liquibase.setLabels(liquibaseProperties.getLabels());
+    liquibase.setChangeLogParameters(liquibaseProperties.getParameters());
+    return liquibase;
+  }
+
+  private DataSource getDataSource(LiquibaseProperties liquibaseProperties) {
+    if (liquibaseProperties.getUrl() == null) {
+      return this.dataSource;
     }
-
-    private DataSource getDataSource(LiquibaseProperties liquibaseProperties) {
-        if (liquibaseProperties.getUrl() == null) {
-            return this.dataSource;
-        }
-        return DataSourceBuilder.create().url(liquibaseProperties.getUrl())
-            .username(liquibaseProperties.getUser())
-            .password(liquibaseProperties.getPassword()).build();
-    }
-
+    return DataSourceBuilder.create().url(liquibaseProperties.getUrl())
+        .username(liquibaseProperties.getUser())
+        .password(liquibaseProperties.getPassword()).build();
+  }
 
 
 }
