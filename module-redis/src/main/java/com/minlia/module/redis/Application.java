fb471dd5f73ddcce3804ea445f3733634311d0d0
will <will@minlia.com>
2017-09-05 17:32:42 +0800

    Update at 20170905173238 by dev/user

--- module-redis/src/main/java/com/minlia/module/redis/Application.java
+++ module-redis/src/main/java/com/minlia/module/redis/Application.java
@@ -1,5 +1,7 @@
 package com.minlia.module.redis;
 
+import com.google.common.collect.Maps;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
