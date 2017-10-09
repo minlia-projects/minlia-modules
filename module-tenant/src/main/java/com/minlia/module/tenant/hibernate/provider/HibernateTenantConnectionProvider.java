333743621711a5066ada4568fbe4697a5366ceb6
will <will@minlia.com>
2017-10-09 20:02:29 +0800

    UPdate

--- module-tenant/src/main/java/com/minlia/module/tenant/hibernate/provider/HibernateTenantConnectionProvider.java
+++ module-tenant/src/main/java/com/minlia/module/tenant/hibernate/provider/HibernateTenantConnectionProvider.java
@@ -1,6 +1,7 @@
 package com.minlia.module.tenant.hibernate.provider;
 
-import com.minlia.module.tenant.hibernate.TenantThreadLocal;
+import com.minlia.module.tenant.resolver.ThreadLocalBatisTenantIdentifierResolver;
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.Map;
 import javax.sql.DataSource;
@@ -8,9 +9,12 @@ import org.hibernate.engine.jdbc.connections.internal.DatasourceConnectionProvid
 import org.hibernate.engine.jdbc.connections.spi.AbstractMultiTenantConnectionProvider;
 import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
 
+/**
+ * JPA多租户连接提供方
+ */
 public class TenantConnectionProvider extends AbstractMultiTenantConnectionProvider {
 
-  private static final long serialVersionUID = -4838344013290465388L;
+  private static final long serialVersionUID = 0L;
 
   private Map<String, DataSource> dataSourceMap;
 
@@ -20,7 +24,7 @@ public class TenantConnectionProvider extends AbstractMultiTenantConnectionProvi
 
   @Override
   protected ConnectionProvider getAnyConnectionProvider() {
-    return getConnectionProvider(TenantThreadLocal.getTenant());
+    return getConnectionProvider(ThreadLocalBatisTenantIdentifierResolver.getCurrentTenantIdentifier());
   }
 
   @Override
@@ -33,6 +37,12 @@ public class TenantConnectionProvider extends AbstractMultiTenantConnectionProvi
 
     DatasourceConnectionProviderImpl connectionProvider = new DatasourceConnectionProviderImpl();
     connectionProvider.setDataSource(dataSource);
+
+    try {
+      connectionProvider.getConnection().setCatalog("");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
     connectionProvider.configure(Collections.emptyMap());
     return connectionProvider;
   }
