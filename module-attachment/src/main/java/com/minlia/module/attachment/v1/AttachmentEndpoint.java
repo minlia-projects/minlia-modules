3a9a5d8771240016db1a41e5d31afd262e8247ea
Henry Lin <badcop@163.com>
2017-10-20 13:26:27 +0800

    update

--- module-attachment/src/main/java/com/minlia/module/attachment/v1/AttachmentEndpoint.java
+++ module-attachment/src/main/java/com/minlia/module/attachment/v1/AttachmentEndpoint.java
@@ -1,11 +1,12 @@
 package com.minlia.module.attachment.v1;
 
-import com.minlia.boot.v1.body.StatefulBody;
-import com.minlia.boot.v1.endpoint.AbstractApiEndpoint;
-import com.minlia.boot.v1.service.IService;
-import com.minlia.boot.v1.web.ApiPrefix;
+import com.minlia.cloud.body.StatefulBody;
+import com.minlia.cloud.body.impl.SuccessResponseBody;
+import com.minlia.cloud.constant.ApiPrefix;
+import com.minlia.module.attachment.v1.body.AttachmentQueryRequestBody;
 import com.minlia.module.attachment.v1.domain.Attachment;
-import com.minlia.module.attachment.v1.service.AttachmentService;
+import com.minlia.module.attachment.v1.service.AttachmentReadOnlyService;
+import com.minlia.module.attachment.v1.service.AttachmentWriteOnlyService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -14,10 +15,8 @@ import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import javax.enterprise.inject.Produces;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by will on 6/21/17.
@@ -26,45 +25,53 @@ import javax.validation.Valid;
 @RequestMapping(value = ApiPrefix.V1 + "attachments")
 @Api(tags = "附件", description = "附件")
 @Slf4j
-public class AttachmentEndpoint extends AbstractApiEndpoint<Attachment>{
-
-    @Override
-    protected IService<Attachment> getService() {
-        return attachmentService;
-    }
+public class AttachmentEndpoint {
 
     @Autowired
-    AttachmentService attachmentService;
+    private AttachmentWriteOnlyService attachmentWriteOnlyService;
 
-    /**
-     * 创建
-     * @return
-     */
-    @PreAuthorize(value = "isAuthenticated()")
-    @ApiOperation(value = "创建", notes = "创建", httpMethod = "POST", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @RequestMapping(value = "create", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public StatefulBody create(@Valid @RequestBody Attachment body) {
-       return super.create(body);
+    @Autowired
+    private AttachmentReadOnlyService attachmentReadOnlyService;
+
+    @PreAuthorize(value = "hasAnyAuthority('tenant.categories.create')")
+    @ApiOperation(value = "添加附件", notes = "添加附件", httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "create", produces = MediaType.APPLICATION_JSON_VALUE)
+    public StatefulBody create(@RequestBody Attachment body) {
+        Attachment created=attachmentWriteOnlyService.save(body);
+        return SuccessResponseBody.builder().payload(created).build();
     }
 
-    /**
-     * 更新
-     */
-    @PreAuthorize(value = "isAuthenticated()")
-    @ApiOperation(value = "更新", notes = "更新", httpMethod = "PUT", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @RequestMapping(value = "update", method = RequestMethod.PUT, consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public StatefulBody update(@Valid @RequestBody Attachment body) {
-        return super.update(body.getId(),body);
+
+    @PreAuthorize(value = "hasAnyAuthority('tenant.categories.update')")
+    @ApiOperation(value = "更新附件", notes = "更新附件", httpMethod = "PUT", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(value = "update", produces = MediaType.APPLICATION_JSON_VALUE)
+    public StatefulBody update(@RequestBody Attachment body) {
+        attachmentWriteOnlyService.update(body);
+        return SuccessResponseBody.builder().payload(body).build();
     }
 
-    /**
-     * 删除
-     */
-    @PreAuthorize(value = "isAuthenticated()")
-    @ApiOperation(value = "删除", notes = "删除", httpMethod = "DELETE", produces = MediaType.APPLICATION_JSON_VALUE)
-    @RequestMapping(value = "{id}", method = RequestMethod.DELETE, produces = {MediaType.APPLICATION_JSON_VALUE})
+//    @PreAuthorize(value = "hasAnyAuthority('tenant.categories.batchUpdate')")
+//    @ApiOperation(value = "批量更新附件", notes = "批量更新附件", httpMethod = "PUT", produces = MediaType.APPLICATION_JSON_VALUE)
+//    @PutMapping(value = "batchUpdate", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public StatefulBody update(@RequestBody Set<Attachment> body) {
+//        AttachmentWriteOnlyService.batchUpdate(body);
+//        return SuccessResponseBody.builder().payload(body).build();
+//    }
+
+    @PreAuthorize(value = "hasAnyAuthority('tenant.categories.delete')")
+    @ApiOperation(value = "删除附件", notes = "删除附件", httpMethod = "DELETE", produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public StatefulBody delete(@PathVariable Long id) {
-        return super.delete(id);
+        attachmentWriteOnlyService.delete(id);
+        return SuccessResponseBody.builder().build();
+    }
+
+    @PreAuthorize(value = "hasAnyAuthority('tenant.categories.find')")
+    @ApiOperation(value = "获取当前应用的所有附件, 不分页", notes = "获取当前应用的所有附件, 不分页", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "findAllList", produces = MediaType.APPLICATION_JSON_VALUE)
+    public StatefulBody findAllList() {
+        List<Attachment> found = attachmentReadOnlyService.findAll();
+        return SuccessResponseBody.builder().payload(found).build();
     }
 
 }
