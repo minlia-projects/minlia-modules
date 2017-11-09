e2df978ca2390d06be54a850ff33ffd9e104c3ee
will <will@minlia.com>
2017-11-09 22:31:27 +0800

    Update

--- module-attachment/src/main/java/com/minlia/module/attachment/v1/code/AttachmentApiCode.java
+++ module-attachment/src/main/java/com/minlia/module/attachment/v1/code/AttachmentApiCode.java
@@ -1,6 +1,7 @@
 package com.minlia.module.attachment.v1.code;
 
-import com.minlia.boot.v1.code.ApiCode;
+
+import com.minlia.cloud.code.ApiCode;
 
 /**
  * Created by will on 6/21/17.
@@ -12,8 +13,8 @@ public class AttachmentApiCode extends ApiCode {
     }
 
     /**
-     * 定义模块代码基数为13000
+     * 定义模块代码基数为15000
      */
-    public static final Integer MODULE_CODE_BASED = BASED + 3000;
+    public static final Integer MODULE_CODE_BASED = BASED_ON + 5000;
 
 }
