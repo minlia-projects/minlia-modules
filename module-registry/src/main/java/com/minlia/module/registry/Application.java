c579c60d9efc9be9ed46e67e3a4ac12de9624c0b
will <will@minlia.com>
2018-01-01 15:09:49 +0800

    Update

--- module-registry/src/main/java/com/minlia/module/registry/Application.java
+++ module-registry/src/main/java/com/minlia/module/registry/Application.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(com.minlia.module.redis.Application.class, args);
+        SpringApplication.run(com.minlia.module.registry.Application.class, args);
     }
 }
