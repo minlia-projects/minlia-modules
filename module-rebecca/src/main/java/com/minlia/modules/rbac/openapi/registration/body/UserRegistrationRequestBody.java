696925d51f289d66a3d217437515e21813cb5e0f
garen <191285052@qq.com>
2018-02-01 21:12:59 +0800

    update

--- module-rebecca/src/main/java/com/minlia/modules/rbac/openapi/registration/body/UserRegistrationRequestBody.java
+++ module-rebecca/src/main/java/com/minlia/modules/rbac/openapi/registration/body/UserRegistrationRequestBody.java
@@ -1,7 +1,8 @@
 package com.minlia.modules.rbac.openapi.registration.body;
 
 import com.minlia.cloud.constant.ValidationConstants;
-import com.minlia.modules.rbac.validation.Cellphone;
+import com.minlia.module.common.validation.Cellphone;
+import com.minlia.module.common.validation.Password;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.hibernate.validator.constraints.NotBlank;
@@ -27,6 +28,7 @@ public class UserRegistrationRequestBody {
    * 密码
    */
   @NotBlank
+  @Password
   @Size(min = ValidationConstants.MIN_SIZE, max = ValidationConstants.MAX_SIZE, message = "Password have to be grater than 8 characters")
   private String password;
 
