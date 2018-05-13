10bc189ccc5da34432b9938abc2d5a1d13fe82de
garen <191285052@qq.com>
2018-05-13 15:33:29 +0800

    Update at 20180513153328 by dev/garen

--- module-redis/src/main/java/com/minlia/module/redis/RedisAutoConfiguration.java
+++ module-redis/src/main/java/com/minlia/module/redis/RedisAutoConfiguration.java
@@ -3,12 +3,16 @@ package com.minlia.module.redis;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.interceptor.SimpleCacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
@@ -95,6 +99,39 @@ public class RedisAutoConfiguration extends CachingConfigurerSupport {
 //        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
+    }
+
+    @Override
+    @Bean
+    public CacheErrorHandler errorHandler() {
+        return new RedisCacheErrorHandler();
+    }
+
+    @Slf4j
+    private static class RedisCacheErrorHandler extends SimpleCacheErrorHandler {
+        @Override
+        public void handleCacheGetError(RuntimeException exception, Cache cache, Object key) {
+            log.error("handleCacheGetError key = {}, value = {}", key, cache);
+            log.error("cache get error", exception);
+        }
+
+        @Override
+        public void handleCachePutError(RuntimeException exception, Cache cache, Object key, Object value) {
+            log.error("handleCachePutError key = {}, value = {}", key, cache);
+            log.error("cache put error", exception);
+        }
+
+        @Override
+        public void handleCacheEvictError(RuntimeException exception, Cache cache, Object key) {
+            log.error("handleCacheEvictError key = {}, value = {}", key, cache);
+            log.error("cache evict error", exception);
+        }
+
+        @Override
+        public void handleCacheClearError(RuntimeException exception, Cache cache) {
+            log.error("handleCacheClearError value = {}", cache);
+            log.error("cache clear error", exception);
+        }
     }
 
 }
