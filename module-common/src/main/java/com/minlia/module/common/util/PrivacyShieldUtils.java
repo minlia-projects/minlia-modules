9aec5f37fd3c8c4ecfb3ce5a56f5676c7efa37d2
garen <191285052@qq.com>
2018-06-29 22:32:53 +0800

    Update at 20180629223253 by dev/garen

--- module-common/src/main/java/com/minlia/module/common/util/PrivacyShieldUtils.java
+++ module-common/src/main/java/com/minlia/module/common/util/PrivacyShieldUtils.java
@@ -3,13 +3,21 @@ package com.minlia.module.common.util;
 import org.apache.commons.lang.StringUtils;
 
 /**
+ * 隐私信息屏蔽
  * Created by garen on 2018/6/14.
  */
 public class PrivacyShieldUtils {
 
+    private static String suffix_id_no_capital = "X";
+    private static String suffix_id_no_lowercase = "x";
+
     public static String idNo(String idNo) {
         if (StringUtils.isNotEmpty(idNo)) {
-            return idNo.replaceAll("(\\d{4})\\d{10}(\\d{4})","$1****$2");
+//            if (idNo.endsWith(suffix_id_no_capital) || idNo.endsWith(suffix_id_no_lowercase)) {
+                return idNo.replaceAll("(\\d{4})\\d{10}(\\d{3})","$1****$2");
+//            } else {
+//                return idNo.replaceAll("(\\d{4})\\d{10}(\\d{4})","$1****$2");
+//            }
         } else {
             return null;
         }
@@ -42,15 +50,16 @@ public class PrivacyShieldUtils {
         return stringBuffer.toString();
     }
 
-//    public static void main(String[] args) {
+    public static void main(String[] args) {
 //        String phone = "18771632488";
 //        System.out.println(phone.replaceAll("(\\d{3})\\d{4}(\\d{4})","$1****$2"));
-//
-//        String idCard = "421302199208165464";
-//        System.out.println(idCard.replaceAll("(\\d{4})\\d{10}(\\d{4})","$1****$2"));
-//
+
+//        String idNo = "421302199208165464";
+        String idNo = "42130219920816546X";
+        System.out.println(idNo(idNo));
+
 //        String name = "后市大范德萨发";
 //        System.out.println(name(name));
-//    }
+    }
 
 }
