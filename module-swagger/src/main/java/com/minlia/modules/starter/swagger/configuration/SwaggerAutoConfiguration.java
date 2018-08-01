913e351c3c448695b0b364a060c2e1c1881274d7
garen <191285052@qq.com>
2018-08-01 14:02:32 +0800

    Update at 20180801140232 by dev/garen

--- module-swagger/src/main/java/com/minlia/modules/starter/swagger/configuration/SwaggerAutoConfiguration.java
+++ module-swagger/src/main/java/com/minlia/modules/starter/swagger/configuration/SwaggerAutoConfiguration.java
@@ -17,25 +17,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Profile(value = {"dev"})
 public class SwaggerAutoConfiguration {
 
-  @Configuration
-  @EnableDevSwagger
+    @Configuration
+    @EnableDevSwagger
 
-  @ConditionalOnMissingBean(EnableDevSwagger.class)
-  public static class Swagger2AutoConfig {
-  }
-
-
-  @Configuration
-  @EnableWebMvc
-  public static class EnableMinliaStaticResourceConfiguration extends WebMvcConfigurerAdapter {
-    private  final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-            "classpath:/META-INF/resources/", "classpath:/resources/",
-            "classpath:/static/", "classpath:/public/" };
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-      registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    @ConditionalOnMissingBean(EnableDevSwagger.class)
+    public static class Swagger2AutoConfig {
     }
-  }
 
+
+    @Configuration
+    @EnableWebMvc
+    public static class EnableMinliaStaticResourceConfiguration extends WebMvcConfigurerAdapter {
+        private final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+                "classpath:/META-INF/resources/", "classpath:/resources/",
+                "classpath:/static/", "classpath:/public/"
+        };
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+        }
+    }
 
 }
