0dd7be485e7dff5b02d49b27d1e4ad164a33aef9
garen <191285052@qq.com>
2018-09-07 02:05:25 +0800

    Update at 20180907020525 by dev/garen

--- module-rebecca/src/main/java/com/minlia/modules/rbac/context/SecurityContextHolder.java
+++ module-rebecca/src/main/java/com/minlia/modules/rbac/context/SecurityContextHolder.java
@@ -28,6 +28,13 @@ public final class SecurityContextHolder {
     public static UserContext getUserContext() {
         SecurityContext securityContext = org.springframework.security.core.context.SecurityContextHolder.getContext();
         Authentication authentication = securityContext.getAuthentication();
+
+        log.info("************************************************************************************" +
+                "**************************************************************************************************5");
+
+        log.info("************************************************************************************" +
+                "**************************************************************************************************5"+ authentication.getPrincipal().toString());
+
         if (authentication != null) {
             if (authentication.getPrincipal() instanceof UserContext) {
                 UserContext userContext = (UserContext) authentication.getPrincipal();
