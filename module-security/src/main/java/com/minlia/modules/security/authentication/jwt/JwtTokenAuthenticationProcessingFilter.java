483f59a4f698855c903172a19d2f13848a1681e6
garen <garen@garen.local>
2018-11-07 20:41:37 +0800

    Update at 20181107204137 by dev/garen

--- module-security/src/main/java/com/minlia/modules/security/authentication/jwt/JwtTokenAuthenticationProcessingFilter.java
+++ module-security/src/main/java/com/minlia/modules/security/authentication/jwt/JwtTokenAuthenticationProcessingFilter.java
@@ -41,13 +41,6 @@ public class JwtTokenAuthenticationProcessingFilter extends AbstractAuthenticati
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
         String tokenPayload = request.getHeader(WebSecurityConfig.JWT_TOKEN_HEADER_PARAM);
-        log.info("**************************************************************************************************************************************");
-        log.info("**************************************************************************************************************************************");
-        log.info("**************************************************************************************************************************************");
-        log.info("jwt url:" + request.getRequestURI());
-        log.info("jwt url:" + request.getRequestURL());
-        log.info("jwt token:" + tokenPayload);
-
         RawAccessJwtToken token = new RawAccessJwtToken(tokenExtractor.extract(tokenPayload));
         return getAuthenticationManager().authenticate(new JwtAuthenticationToken(token));
     }
