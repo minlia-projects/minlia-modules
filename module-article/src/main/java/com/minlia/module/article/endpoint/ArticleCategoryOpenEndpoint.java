6e392967349b2ecbab5f33ef9599c6a40d55e4ce
garen <garen@garen.local>
2018-11-26 20:47:00 +0800

    Update at 20181126204659 by dev/garen

--- module-article/src/main/java/com/minlia/module/article/endpoint/ArticleCategoryOpenEndpoint.java
+++ module-article/src/main/java/com/minlia/module/article/endpoint/ArticleCategoryOpenEndpoint.java
@@ -2,10 +2,9 @@ package com.minlia.module.article.endpoint;
 
 import com.minlia.cloud.body.Response;
 import com.minlia.cloud.constant.ApiPrefix;
-import com.minlia.module.article.bean.qo.ArticleQO;
+import com.minlia.module.article.bean.qo.ArticleCategoryQO;
 import com.minlia.module.article.constant.ArticleConstants;
 import com.minlia.module.article.service.ArticleCategoryService;
-import com.minlia.module.article.service.ArticleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,47 +17,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = "System Article Open", description = "文章-公开")
+@Api(tags = "System Article Category OPEN", description = "文章-类目")
 @RestController
-@RequestMapping(value = ApiPrefix.OPEN + "article")
-public class ArticleOpenEndpoint {
+@RequestMapping(value = ApiPrefix.OPEN + "article/category")
+public class ArticleCategoryOpenEndpoint {
 
 	@Autowired
-	private ArticleService articleService;
+	private ArticleCategoryService articleCategoryService;
 
-//	@PreAuthorize(value = "hasAnyAuthority('" + ArticleConstants.SEARCH + "')")
-//	@ApiOperation(value = "ID查询", notes = "ID查询", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_VALUE)
-//	@RequestMapping(value = "{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
-//	public Response findByNumber(@PathVariable Long id) {
-//		return Response.success(articleService.queryById(id));
-//	}
-//
-//	@PreAuthorize(value = "hasAnyAuthority('" + ArticleConstants.SEARCH + "')")
-//	@ApiOperation(value = "计数查询", notes = "计数查询", httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
-//	@RequestMapping(value = "count", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
-//	public Response count(@RequestBody ArticleQO qo) {
-//		return Response.success(articleService.count(qo));
-//	}
-//
+	@PreAuthorize(value = "hasAnyAuthority('" + ArticleConstants.SEARCH + "')")
+	@ApiOperation(value = "计数查询", notes = "计数查询", httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "count", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public Response count(@RequestBody ArticleCategoryQO qo) {
+		qo.setEnabled(true);
+		return Response.success(articleCategoryService.count(qo));
+	}
+
+	@PreAuthorize(value = "hasAnyAuthority('" + ArticleConstants.SEARCH + "')")
 	@ApiOperation(value = "单个查询", notes = "单个查询", httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = "one", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
-	public Response one(@RequestBody ArticleQO qo) {
+	public Response one(@RequestBody ArticleCategoryQO qo) {
 		qo.setEnabled(true);
-		return Response.success(articleService.one(qo));
+		return Response.success(articleCategoryService.one(qo));
 	}
 
+	@PreAuthorize(value = "hasAnyAuthority('" + ArticleConstants.SEARCH + "')")
 	@ApiOperation(value = "集合查询", notes = "编号查询", httpMethod = "POST", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = "list", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-	public Response list(@RequestBody ArticleQO qo) {
+	public Response list(@RequestBody ArticleCategoryQO qo) {
 		qo.setEnabled(true);
-		return Response.success(articleService.listVO(qo));
+		return Response.success(articleCategoryService.list(qo));
 	}
 
+	@PreAuthorize(value = "hasAnyAuthority('" + ArticleConstants.SEARCH + "')")
 	@ApiOperation(value = "分页查询", notes = "编号查询", httpMethod = "POST", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = "page", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-	public Response page(@PageableDefault Pageable pageable, @RequestBody ArticleQO qo) {
+	public Response page(@PageableDefault Pageable pageable, @RequestBody ArticleCategoryQO qo) {
 		qo.setEnabled(true);
-		return Response.success(articleService.pageVO(qo, pageable));
+		return Response.success(articleCategoryService.page(qo, pageable));
 	}
 
 }
