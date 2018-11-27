08924074a98c68ee8e8c60941b2dc00b360115e3
garen <garen@garen.local>
2018-11-27 21:20:32 +0800

    Update at 20181127212032 by dev/garen

--- module-data/src/main/java/com/minlia/module/data/body/QueryRequest.java
+++ module-data/src/main/java/com/minlia/module/data/body/QueryRequest.java
@@ -2,29 +2,47 @@ package com.minlia.module.data.body;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.minlia.cloud.body.Body;
 import com.minlia.module.common.constant.SymbolConstants;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 import java.util.StringJoiner;
 
 /**
  * Created by garem on 2017/7/15
- * 标识为搜索请求体
+ * TODO  要不要把分页放这里
  */
 public class QueryRequest implements Body {
 
     @JsonIgnore
     private String query;
 
+    @JsonIgnore
     private Integer page;
 
+    @JsonIgnore
     private Integer size;
 
     //排序
     private List<SortBody> sorts;
 
-    public String getSorts() {
+    @Data
+    public static class SortBody {
+
+        @ApiModelProperty(example = "id")
+        private String filed;
+
+        @ApiModelProperty(example = "DESC")
+        private Sort.Direction direction;
+
+    }
+
+    @JsonProperty("sort")
+    public String getSort() {
         StringJoiner sj = new StringJoiner(SymbolConstants.COMMA);
         for (SortBody sort : sorts) {
             StringBuilder sb = new StringBuilder();
--- module-data/src/main/java/com/minlia/module/data/body/SortBody.java
+++ module-data/src/main/java/com/minlia/module/data/body/SortBody.java
@@ -1,19 +1,19 @@
-package com.minlia.module.data.body;
-
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
-import org.springframework.data.domain.Sort;
-
-/**
- * Created by will on 3/19/17.
- */
-@Data
-public class SortBody {
-
-    @ApiModelProperty(example = "id")
-    private String filed;
-
-    @ApiModelProperty(example = "DESC")
-    private Sort.Direction direction;
-
-}
+//package com.minlia.module.data.body;
+//
+//import io.swagger.annotations.ApiModelProperty;
+//import lombok.Data;
+//import org.springframework.data.domain.Sort;
+//
+///**
+// * Created by will on 3/19/17.
+// */
+//@Data
+//public class SortBody {
+//
+//    @ApiModelProperty(example = "id")
+//    private String filed;
+//
+//    @ApiModelProperty(example = "DESC")
+//    private Sort.Direction direction;
+//
+//}
