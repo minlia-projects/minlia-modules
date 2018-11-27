08c612f568231477f7216e54708fb7d301fd941a
garen <garen@garen.local>
2018-11-27 21:08:59 +0800

    Update at 20181127210859 by dev/garen

--- module-data/src/main/java/com/minlia/module/data/body/QueryRequest.java
+++ module-data/src/main/java/com/minlia/module/data/body/QueryRequest.java
@@ -1,13 +1,37 @@
 package com.minlia.module.data.body;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.minlia.cloud.body.Body;
+import com.minlia.module.common.constant.SymbolConstants;
+
+import java.util.List;
+import java.util.StringJoiner;
 
 /**
- * Created by will on 7/5/17.
+ * Created by garem on 2017/7/15
  * 标识为搜索请求体
  */
-public interface QueryRequest extends Body {
+public class QueryRequest implements Body {
 
+    @JsonIgnore
+    private String query;
+
+    private Integer page;
+
+    private Integer size;
+
+    //排序
+    private List<SortBody> sorts;
+
+    public String getSorts() {
+        StringJoiner sj = new StringJoiner(SymbolConstants.COMMA);
+        for (SortBody sort : sorts) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(sort.getFiled()).append(SymbolConstants.SPACE).append(sort.getDirection().name());
+            sj.add(sb.toString());
+        }
+        return sj.toString();
+    }
 
 }
