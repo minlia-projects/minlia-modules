3bcc3a503698a5ad457466c6961ffc3307d755af
will <will@chinfan.io>
2019-06-27 17:49:13 +0800

    Update otp audit log

--- module-captcha/src/main/java/com/minlia/module/captcha/endpoint/CaptchaOpenEndpoint.java
+++ module-captcha/src/main/java/com/minlia/module/captcha/endpoint/CaptchaOpenEndpoint.java
@@ -4,6 +4,7 @@ import com.minlia.cloud.body.Response;
 import com.minlia.cloud.constant.ApiPrefix;
 import com.minlia.cloud.utils.ApiAssert;
 import com.minlia.cloud.utils.Environments;
+import com.minlia.module.audit.annotation.AuditLog;
 import com.minlia.module.captcha.constant.CaptchaCode;
 import com.minlia.module.captcha.entity.Captcha;
 import com.minlia.module.captcha.enumeration.CaptchaMethodEnum;
@@ -47,6 +48,7 @@ public class CaptchaOpenEndpoint {
 //        }
 //    }
 
+    @AuditLog(value = "send otp")
     @ApiOperation(value = "发送", notes = "发送", httpMethod = "POST", consumes = MediaType.APPLICATION_JSON_VALUE)
     @RequestMapping(value = "send", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public Response send(@Valid @RequestBody CaptchaCRO cro) {
@@ -59,6 +61,7 @@ public class CaptchaOpenEndpoint {
         }
     }
 
+    @AuditLog(value = "verify otp")
     @ApiOperation(value = "验证", notes = "验证", httpMethod = "POST", consumes = MediaType.APPLICATION_JSON_VALUE)
     @RequestMapping(value = "verify", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public Response verify(@Valid @RequestBody CaptchaVerifyRO verifyRO) {
