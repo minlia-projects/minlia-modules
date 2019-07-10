d0deb6f5082fa639b9c965a6e17b15dd5c2f3c06
garen <191285052@qq.com>
2019-07-10 15:02:31 +0800

    update

---
+++ riskcontrol-spring-boot-starter/src/main/java/com/minlia/module/riskcontrol/event/RiskBlackIpEvent.java
@@ -0,0 +1,13 @@
+package com.minlia.module.riskcontrol.event;
+
+import lombok.Data;
+
+@Data
+public class RiskBlackIpEvent extends Event {
+
+    public RiskBlackIpEvent() {
+        super();
+        setScene("black_ip");
+    }
+
+}
---
+++ riskcontrol-spring-boot-starter/src/main/java/com/minlia/module/riskcontrol/filter/BlackListOncePreFilter.java
@@ -0,0 +1,42 @@
+package com.minlia.module.riskcontrol.filter;
+
+import com.minlia.cloud.holder.ContextHolder;
+import com.minlia.cloud.utils.ApiAssert;
+import com.minlia.module.drools.service.ReloadDroolsRulesService;
+import com.minlia.module.riskcontrol.constant.RiskCode;
+import com.minlia.module.riskcontrol.event.RiskBlackIpEvent;
+import com.minlia.module.riskcontrol.service.RiskBlackListService;
+import org.kie.api.runtime.StatelessKieSession;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Order(1)
+@Component
+@WebFilter(urlPatterns = "/*", filterName = "blackListOncePreFilter")
+public class BlackListOncePreFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        StatelessKieSession kieSession = ReloadDroolsRulesService.kieContainer.newStatelessKieSession();
+        RiskBlackListService riskBlackListService = ContextHolder.getContext().getBean(RiskBlackListService.class);
+        kieSession.setGlobal("riskBlackListService", riskBlackListService);
+        RiskBlackIpEvent riskBlackIpEvent = new RiskBlackIpEvent();
+        kieSession.execute(riskBlackIpEvent);
+        ApiAssert.state(!riskBlackIpEvent.isBlack(), RiskCode.Message.BLACK_IP.code(), RiskCode.Message.BLACK_IP.i18nKey());
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
