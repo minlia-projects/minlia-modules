54a7ad7b16620d8bfc18b3b1cab59abcac48330e
garen <191285052@qq.com>
2019-07-31 16:57:25 +0800

    update

---
+++ module-common/src/main/java/com/minlia/module/common/constant/LocalDateConstants.java
@@ -0,0 +1,14 @@
+package com.minlia.module.common.constant;
+
+/**
+ * @author garen
+ * @version 1.0
+ * @description
+ * @date 2019/7/31 11:50 AM
+ */
+public class LocalDateConstants {
+
+    public static final String DEFAULT_LOCAL_DATE_FORMAT = "yyyy-MM-dd";
+    public static final String DEFAULT_LOCAL_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+}
