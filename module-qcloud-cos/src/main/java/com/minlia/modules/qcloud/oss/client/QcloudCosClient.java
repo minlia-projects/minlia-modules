1c8cb13d8bd84bec3378e3c978f436ddcfb90685
garen <191285052@qq.com>
2019-09-10 14:35:47 +0800

    update

--- module-qcloud-cos/src/main/java/com/minlia/modules/qcloud/oss/client/QcloudCosClient.java
+++ module-qcloud-cos/src/main/java/com/minlia/modules/qcloud/oss/client/QcloudCosClient.java
@@ -20,12 +20,12 @@ public class QcloudCosClient {
             QcloudCosConfig properties = ContextHolder.getContext().getBean(QcloudCosConfig.class);
             // 1 初始化用户身份信息(secretId, secretKey)
             COSCredentials cred = new BasicCOSCredentials(properties.getAccessKey(), properties.getSecretKey());
-            // 2 设置bucket的区域, COS地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
-            ClientConfig clientConfig = new ClientConfig(new Region(properties.getRegion()));
-            // 3 生成cos客户端
+            // 2 设置 bucket 的区域, COS 地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
+            // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
+            Region region = new Region(properties.getRegion());
+            ClientConfig clientConfig = new ClientConfig(region);
+            // 3 生成 cos 客户端。
             cosClient = new COSClient(cred, clientConfig);
-            // bucket的命名规则为{name}-{appid} ，此处填写的存储桶名称必须为此格式w
-            String bucketName = properties.getBucketName();
         }
         return cosClient;
     }
