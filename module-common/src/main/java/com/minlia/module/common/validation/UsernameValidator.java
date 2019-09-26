91c006f7d04f47b218da2cf6553c5f5e1ca7a03e
garen <191285052@qq.com>
2019-09-26 11:34:03 +0800

    update

--- module-common/src/main/java/com/minlia/module/common/validation/UsernameValidator.java
+++ module-common/src/main/java/com/minlia/module/common/validation/UsernameValidator.java
@@ -20,7 +20,7 @@ public class UsernameValidator implements ConstraintValidator<Username, String> 
     @Override
     public boolean isValid(String username, ConstraintValidatorContext constraintValidatorContext) {
         if (StringUtils.isNotEmpty(username)) {
-            return Pattern.matches("^[a-zA-z][a-zA-Z0-9_]{2,9}$",username);
+            return Pattern.matches("^[a-zA-z][a-zA-Z0-9_]{2,50}$",username);
         } else {
             return true;
         }
