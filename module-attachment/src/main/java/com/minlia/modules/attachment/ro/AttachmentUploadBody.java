2c4be681165da643b0a731844ef6d6bef6941b41
garen <191285052@qq.com>
2019-10-11 16:07:46 +0800

    update

---
+++ module-attachment/src/main/java/com/minlia/modules/attachment/ro/AttachmentUploadBody.java
@@ -0,0 +1,42 @@
+package com.minlia.modules.attachment.ro;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotBlank;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+
+/**
+ * Created by garen on 2018/6/6.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AttachmentUploadBody {
+
+    @NotBlank
+    private String file;
+
+    @NotBlank
+    private String contentType;
+
+    @NotBlank
+    private String originalFilename;
+
+
+    @JsonIgnore
+    private String fileName;
+
+    @JsonIgnore
+    private Long size;
+
+    @JsonIgnore
+    private ByteArrayInputStream inputStream;
+
+}
