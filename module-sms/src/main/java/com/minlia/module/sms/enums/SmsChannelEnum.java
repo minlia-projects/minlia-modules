ecd7cdf8a027d62f930ba0d829f0ee6a5ae8cac8
garen <191285052@qq.com>
2019-11-26 16:44:51 +0800

    update

---
+++ module-sms/src/main/java/com/minlia/module/sms/enums/SmsChannelEnum.java
@@ -0,0 +1,17 @@
+package com.minlia.module.sms.enums;
+
+/**
+ * @author garen
+ * @version 1.0
+ * @description 短信通道枚举
+ * @date 2019/11/26 3:20 PM
+ */
+public enum SmsChannelEnum {
+
+    ALIYUN,
+
+    QCLOUD,
+
+    OPT
+
+}
