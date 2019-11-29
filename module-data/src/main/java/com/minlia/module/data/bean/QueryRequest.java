60f59e1b5ff24218b9a1f27fb6ad433c74669205
garen <191285052@qq.com>
2019-11-29 14:16:52 +0800

    update

--- module-data/src/main/java/com/minlia/module/data/bean/QueryRequest.java
+++ module-data/src/main/java/com/minlia/module/data/bean/QueryRequest.java
@@ -1,33 +1,29 @@
 package com.minlia.module.data.bean;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.CaseFormat;
-import com.google.common.collect.Sets;
 import com.minlia.cloud.body.Body;
+import com.minlia.cloud.utils.ApiAssert;
 import com.minlia.module.common.constant.SymbolConstants;
-import lombok.Data;
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 
-import java.util.Set;
-import java.util.StringJoiner;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 /**
  * Created by garem on 2017/7/15
  * TODO  要不要把分页放这里
  */
-@Data
 public class QueryRequest implements Body {
 
-    @JsonIgnore
-    private String query;
-
     //    @JsonIgnore
+    @Min(0)
     private Integer page;
 
     //    @JsonIgnore
+    @Max(3000)
     private Integer size;
 
     public QueryRequest() {
@@ -35,41 +31,8 @@ public class QueryRequest implements Body {
         size = 15;
     }
 
-    //排序参数，前端传过来的 TODO
     @JsonProperty("sorts")
     private String sortsStr;
-
-    @JsonIgnore
-    private Set<Sort> sorts;
-
-    public Set<Sort> getSorts() {
-        if (null == sorts) {
-            sorts = splitSortsStr(sortsStr);
-        }
-        return sorts;
-    }
-
-    public void addSort(String filed, Sort.Direction direction) {
-        if (null == sorts) {
-            sorts = Sets.newHashSet();
-        }
-        sorts.add(new Sort(filed, direction));
-    }
-
-    private static Set<Sort> splitSortsStr(String sortsStr) {
-        if (StringUtils.isNotBlank(sortsStr)) {
-            Set<Sort> set = Sets.newHashSet();
-            String[] sortsArray = sortsStr.split(SymbolConstants.ZHX);
-            for (int i = 0; i < sortsArray.length; i++) {
-                String[] sortArray = sortsArray[i].split(SymbolConstants.DOT_ZY);
-                Sort sort = new Sort(sortArray[0], Sort.Direction.valueOf(sortArray[1]));
-                set.add(sort);
-            }
-            return set;
-        } else {
-            return null;
-        }
-    }
 
     public int getPageSize() {
         return this.size;
@@ -79,22 +42,25 @@ public class QueryRequest implements Body {
         return this.page;
     }
 
+    public String getSortsStr() {
+        return sortsStr;
+    }
 
-//    @JsonProperty("sort")
-//    public String getSort() {
-//        if (CollectionUtils.isNotEmpty(sorts)) {
-//            StringJoiner sj = new StringJoiner(SymbolConstants.COMMA);
-//            for (Sort sort : sorts) {
-//                StringBuilder sb = new StringBuilder();
-//                sb.append(sort.getFiled()).append(SymbolConstants.SPACE).append(sort.getDirection().name());
-//                sj.add(sb.toString());
-//            }
-//            return sj.toString();
-//        } else {
-//            return null;
-//        }
-//    }
+    @Value("${system.pagination.minimum-page-number:0}")
+    public Integer minimumPageSize;
 
+    @Value("${system.pagination.maximumPageSize:2000}")
+    public Integer maximumPageSize;
+
+    public void setPage(Integer page) {
+        ApiAssert.state(size < minimumPageSize, "MINIMUM_PAGINATION_NUMBER", "Minimum value cannot be less than" + minimumPageSize);
+        this.page = page;
+    }
+
+    public void setSize(Integer size) {
+        ApiAssert.state(size > maximumPageSize, "MAXIMUM_PAGINATION_SIZE", "Maximum value cannot be greater than" + maximumPageSize);
+        this.size = size;
+    }
 
     private static final String DOT_ASC = ".ASC";
     private static final String DOT_DESC = ".DESC";
@@ -103,14 +69,12 @@ public class QueryRequest implements Body {
     private static final String SPACE_DESC = " desc";
 
     public String getOrderBy() {
-        //id.DESC-name.ASC to id DESC,name ASC
         return StringUtils.isNotBlank(sortsStr) ? CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, sortsStr.replace(DOT_ASC, SPACE_ASC).replace(DOT_DESC, SPACE_DESC).replace(SymbolConstants.ZHX, SymbolConstants.COMMA)) : null;
     }
-
 
     public static void main(String[] args) {
         String sortsStr = "id.DESC-name.ASC";
         System.out.println(CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, sortsStr.replace(DOT_ASC, SPACE_ASC).replace(DOT_DESC, SPACE_DESC).replace(SymbolConstants.ZHX, SymbolConstants.COMMA)));
-
     }
+
 }
