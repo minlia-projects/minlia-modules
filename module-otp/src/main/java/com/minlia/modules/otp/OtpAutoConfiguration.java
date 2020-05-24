b728f3e05d1374ffb5ed6ca0c12ea0e580eaa1af
garen <191285052@qq.com>
2020-05-24 18:44:47 +0800

    updare

--- module-otp/src/main/java/com/minlia/modules/otp/OtpAutoConfiguration.java
+++ module-otp/src/main/java/com/minlia/modules/otp/OtpAutoConfiguration.java
@@ -1,32 +1,32 @@
-//package com.minlia.modules.otp;
-//
-//import com.minlia.modules.otp.sms.OtpSmsProperties;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.boot.context.properties.EnableConfigurationProperties;
-//import org.springframework.boot.context.properties.bind.BindResult;
-//import org.springframework.boot.context.properties.bind.Binder;
-//import org.springframework.boot.context.properties.source.ConfigurationPropertySource;
-//import org.springframework.boot.context.properties.source.ConfigurationPropertySources;
-//import org.springframework.context.EnvironmentAware;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.env.Environment;
-//
-//import java.util.Properties;
-//
-//@Slf4j
-//@Configuration
-//@EnableConfigurationProperties(value = {OtpSmsProperties.class})
-//public class OtpAutoConfiguration implements EnvironmentAware {
-//
-//    private Properties properties;
-//
-//    @Override
-//    public void setEnvironment(Environment environment) {
-//        Iterable<ConfigurationPropertySource> sources = ConfigurationPropertySources.get(environment);
-//        Binder binder = new Binder(sources);
-//        BindResult<Properties> bindResult = binder.bind("system.sms.otp", Properties.class);
-//        Properties properties = bindResult.get();
-//        this.properties = properties;
-//    }
-//
-//}
+package com.minlia.modules.otp;
+
+import com.minlia.modules.otp.sms.OtpSmsProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.bind.BindResult;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.boot.context.properties.source.ConfigurationPropertySource;
+import org.springframework.boot.context.properties.source.ConfigurationPropertySources;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.Properties;
+
+@Slf4j
+@Configuration
+@EnableConfigurationProperties(value = {OtpSmsProperties.class})
+public class OtpAutoConfiguration implements EnvironmentAware {
+
+    private Properties properties;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        Iterable<ConfigurationPropertySource> sources = ConfigurationPropertySources.get(environment);
+        Binder binder = new Binder(sources);
+        BindResult<Properties> bindResult = binder.bind("system.sms.otp", Properties.class);
+        Properties properties = bindResult.get();
+        this.properties = properties;
+    }
+
+}
