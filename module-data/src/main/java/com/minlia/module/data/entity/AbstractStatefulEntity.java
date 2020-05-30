3d5ad393f8cc7dd4f40bec521bf3ec038b5b3966
will <will@minlia.com>
2020-05-30 21:08:54 +0800

    Updated for auditable entity and identity entity

--- module-data/src/main/java/com/minlia/module/data/entity/AbstractStatefulEntity.java
+++ module-data/src/main/java/com/minlia/module/data/entity/AbstractStatefulEntity.java
@@ -22,7 +22,7 @@ import javax.persistence.MappedSuperclass;
 @NoArgsConstructor
 @SuperBuilder
 public abstract class AbstractStatefulEntity extends AbstractIdentifiableEntity implements StatefulType {
-    @Column(name = "data_status",  columnDefinition = "TINYINT DEFAULT 1 COMMENT 'Data status: 0: invalid, 1: OK, 2: enabled, 3: disabled, 4: deleted, 5: expired, 6: archived'")
+    @Column(name = "data_status",  columnDefinition = "TINYINT(1) DEFAULT 1 COMMENT 'Data status: 0: invalid, 1: OK, 2: enabled, 3: disabled, 4: deleted, 5: expired, 6: archived'")
     private DataStatusEnum dataStatus;
 
 }
