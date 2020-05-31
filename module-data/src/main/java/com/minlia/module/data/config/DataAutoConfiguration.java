7d18ae8a34275419c909fa7a846f7891940a0a8f
will <will@minlia.com>
2020-05-31 10:54:41 +0800

    Updated for auditable entity and identity entity

--- module-data/src/main/java/com/minlia/module/data/config/DataAutoConfiguration.java
+++ module-data/src/main/java/com/minlia/module/data/config/DataAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.minlia.module.data.config;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,7 +13,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @ComponentScan(basePackages = "com.minlia.*")
 @MapperScan(basePackages={"com.minlia.**.mapper"})
-
 @Configuration
 @ConditionalOnClass(com.minlia.cloud.autoconfiguration.MinliaCloudAutoConfiguration.class)
 @EnableTransactionManagement
