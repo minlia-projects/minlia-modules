a09cddd25f98cfa2d1a5dc85dd06205aaccadc53
will <will@minlia.com>
2020-06-16 15:42:33 +0800

    Updated nestedset model

--- module-nestedset/src/main/java/com/minlia/module/nestedset/config/Configuration.java
+++ module-nestedset/src/main/java/com/minlia/module/nestedset/config/Configuration.java
@@ -4,7 +4,7 @@ package com.minlia.module.nestedset.config;
  * #%L
  * minlia
  * %%
- * Copyright (C) 2005 - 2020 Minlia, Inc
+ * Copyright (C) 2005 - 2020 Minlia Team
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
