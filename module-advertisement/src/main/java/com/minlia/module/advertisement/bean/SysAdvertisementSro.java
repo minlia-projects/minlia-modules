2f3ee9fd5ae7b45c0f2390f6cf1a3889bff41edf
garenmax <191285052@qq.com>
2021-05-21 23:27:33 +0800

    updsate

--- module-advertisement/src/main/java/com/minlia/module/advertisement/bean/SysAdvertisementSro.java
+++ module-advertisement/src/main/java/com/minlia/module/advertisement/bean/SysAdvertisementSro.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.Size;
 /**
  * @author garen
  */
-@ApiModel(value = "AdvertisementSro")
+@ApiModel(value = "SysAdvertisementSro")
 @Data
 public class SysAdvertisementSro implements ApiRequestBody {
 
