32e1d05fda934f44b48906873d33e8bb14f334c4
garen <191285052@qq.com>
2021-12-24 15:06:04 +0800

    update

--- minlia-member/minlia-member-biz/src/main/java/com/minlia/module/member/constant/SysMemberConstants.java
+++ minlia-member/minlia-member-biz/src/main/java/com/minlia/module/member/constant/SysMemberConstants.java
@@ -9,9 +9,6 @@ import com.minlia.modules.security.constant.SecurityConstant;
 public class SysMemberConstants {
 
     public final static String MODULE_NAME = "member";
-    public final static String SMS_ENTERPRISE_CERTIFICATION_PASSED = "ENTERPRISE_CERTIFICATION_PASSED";
-    public final static String SMS_ENTERPRISE_CERTIFICATION_FAIL = "ENTERPRISE_CERTIFICATION_FAIL";
-    public final static String EMAIL_ENTERPRISE_CERTIFICATION_APPLICATION = "ENTERPRISE_CERTIFICATION_APPLICATION";
 
     public static class Authorize {
         public final static String READ = MODULE_NAME + SecurityConstant.OPERATION_READ_CODE;
