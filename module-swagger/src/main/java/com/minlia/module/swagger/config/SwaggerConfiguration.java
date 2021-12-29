dc71a9cbe94473a7c103b69d6f735577a1061cea
garen <191285052@qq.com>
2021-12-29 16:00:56 +0800

    update

--- module-swagger/src/main/java/com/minlia/module/swagger/config/SwaggerConfiguration.java
+++ module-swagger/src/main/java/com/minlia/module/swagger/config/SwaggerConfiguration.java
@@ -1,12 +1,11 @@
 package com.minlia.module.swagger.config;
 
-import com.google.common.collect.Sets;
 import com.minlia.module.swagger.properties.SwaggerProperties;
-import io.swagger.annotations.Api;
 import io.swagger.models.auth.In;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.springframework.boot.SpringBootVersion;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
@@ -14,13 +13,9 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.*;
-import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
 
 import java.lang.reflect.Field;
 import java.util.Collections;
@@ -33,43 +28,16 @@ import java.util.List;
  */
 @EnableOpenApi
 @Configuration
+@AllArgsConstructor
+@Getter
 public class SwaggerConfiguration implements WebMvcConfigurer {
 
     private final SwaggerProperties swaggerProperties;
 
-    public SwaggerConfiguration(SwaggerProperties swaggerProperties) {
-        this.swaggerProperties = swaggerProperties;
-    }
-
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.OAS_30).pathMapping("/")
-                .groupName("全部")
-                // 定义是否开启swagger，false为关闭，可以通过变量控制
-                .enable(swaggerProperties.getEnable())
-//                .groupName()
-                // 将api的元信息设置为包含在json ResourceListing响应中。
-                .apiInfo(apiInfo())
-                // 接口调试地址
-                .host(swaggerProperties.getHost())
-                // 选择哪些接口作为swagger的doc发布
-                .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
-                .paths(PathSelectors.any())
-                .build()
-
-                // 支持的通讯协议集合
-                .protocols(Sets.newHashSet("https", "http"))
-                // 授权信息设置，必要的header token等认证信息
-                .securitySchemes(securitySchemes())
-                // 授权信息全局应用
-                .securityContexts(securityContexts());
-    }
-
     /**
      * API 页面上半部分展示信息
      */
-    private ApiInfo apiInfo() {
+    public ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title(swaggerProperties.getTitle() + " Api Doc")
                 .description(swaggerProperties.getDescription())
@@ -82,7 +50,7 @@ public class SwaggerConfiguration implements WebMvcConfigurer {
     /**
      * 设置授权信息
      */
-    private List<SecurityScheme> securitySchemes() {
+    public List<SecurityScheme> securitySchemes() {
         ApiKey apiKey = new ApiKey("X-Auth-Token", "token", In.HEADER.toValue());
         return Collections.singletonList(apiKey);
     }
@@ -90,7 +58,7 @@ public class SwaggerConfiguration implements WebMvcConfigurer {
     /**
      * 授权信息全局应用
      */
-    private List<SecurityContext> securityContexts() {
+    public List<SecurityContext> securityContexts() {
         return Collections.singletonList(
                 SecurityContext.builder()
                         .securityReferences(Collections.singletonList(new SecurityReference("X-Auth-Token", new AuthorizationScope[]{new AuthorizationScope("global", "")})))
@@ -100,6 +68,7 @@ public class SwaggerConfiguration implements WebMvcConfigurer {
 
     /**
      * 通用拦截器排除swagger设置，所有拦截器都会自动加swagger相关的资源排除信息
+     *
      * @param registry xx
      */
     @Override
