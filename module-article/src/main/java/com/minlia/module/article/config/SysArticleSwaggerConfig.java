127a025a4bd39af0f61263e87754a314f0aae6d0
garen <191285052@qq.com>
2021-12-29 17:30:43 +0800

    update

--- module-article/src/main/java/com/minlia/module/article/config/SysArticleSwaggerConfig.java
+++ module-article/src/main/java/com/minlia/module/article/config/SysArticleSwaggerConfig.java
@@ -1,80 +1,43 @@
 package com.minlia.module.article.config;
 
 import com.google.common.collect.Sets;
+import com.minlia.module.swagger.config.SwaggerConfiguration;
 import com.minlia.module.swagger.properties.SwaggerProperties;
 import io.swagger.annotations.Api;
-import io.swagger.models.auth.In;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.SpringBootVersion;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
+import org.springframework.stereotype.Component;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.Collections;
-import java.util.List;
 
 /**
  * @author garen
  */
-@Configuration
-@RequiredArgsConstructor
-public class SysArticleSwaggerConfig {
+@Component
+public class SysArticleSwaggerConfig extends SwaggerConfiguration {
 
-    private final SwaggerProperties swaggerProperties;
+    public SysArticleSwaggerConfig(SwaggerProperties swaggerProperties) {
+        super(swaggerProperties);
+    }
 
     @Bean
     public Docket webApiArticle() {
         return new Docket(DocumentationType.OAS_30)
                 .groupName("文章")
                 .pathMapping("/")
-                .enable(swaggerProperties.getEnable())
+                .enable(getSwaggerProperties().getEnable())
                 .apiInfo(apiInfo())
-                .host(swaggerProperties.getHost())
+                .host(getSwaggerProperties().getHost())
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
                 .paths(PathSelectors.ant("/api/**/article/**"))
                 .build()
+                .directModelSubstitute(Long.class, String.class)
+                .directModelSubstitute(long.class, String.class)
                 .protocols(Sets.newHashSet("https", "http"))
                 .securitySchemes(securitySchemes())
                 .securityContexts(securityContexts());
-    }
-
-    /**
-     * API 页面上半部分展示信息
-     */
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title(swaggerProperties.getTitle() + " Api Doc")
-                .description(swaggerProperties.getDescription())
-                .termsOfServiceUrl(swaggerProperties.getTermsOfServiceUrl())
-                .contact(new Contact(swaggerProperties.contact.getName(), swaggerProperties.contact.getUrl(), swaggerProperties.contact.getEmail()))
-                .version("Application Version: " + swaggerProperties.getVersion() + ", Spring Boot Version: " + SpringBootVersion.getVersion())
-                .build();
-    }
-
-    /**
-     * 设置授权信息
-     */
-    private List<SecurityScheme> securitySchemes() {
-        ApiKey apiKey = new ApiKey("X-Auth-Token", "token", In.HEADER.toValue());
-        return Collections.singletonList(apiKey);
-    }
-
-    /**
-     * 授权信息全局应用
-     */
-    private List<SecurityContext> securityContexts() {
-        return Collections.singletonList(
-                SecurityContext.builder()
-                        .securityReferences(Collections.singletonList(new SecurityReference("X-Auth-Token", new AuthorizationScope[]{new AuthorizationScope("global", "")})))
-                        .build()
-        );
     }
 
 }
